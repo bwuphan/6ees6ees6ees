6d5df83a69b7070d2da3853042f68995f4ac05c3
bobby <bwuphan@gmail.com>
2017-01-06 10:45:55 -0800

    Finished basic requirements

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee{
   // TODO..
+  constructor() {
+    super();
+    this.age = 10;
+    this.canFly = true;
+    this.treasureChest = [];
+    this.job = 'find pollen';
+  }
+  forage(treasure){
+    this.treasureChest.push(treasure);
+  }
 };
